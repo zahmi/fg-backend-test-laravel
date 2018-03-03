7dc6344ebb25851d350f58fc72b728ebed9333cd
Zahmi <zahmiz@ellipsis.lk>
2018-03-03 12:44:56 +0530

    created table migrations for author and article

---
+++ database/migrations/2018_03_03_071001_create_article_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('author_id')->unsigned();
+            $table->foreign('author_id')->references('id')->on('author');
+            $table->string('title', 255);
+            $table->string('url')->unique();
+            $table->text('content');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
